d59bdfa03ffa74107bc3439efe189a12cbd4f589
MariaJJ20205 <161511235+MariaJJ20205@users.noreply.github.com>
2024-09-12 19:47:27 -0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,26 @@
+let nombre = "Lupe"
+let apellido = "Jiménez"
+let edad = 5
+let estudio = "Enfermera"
+let apodo = "Bebé"
+let estaturacm = 50
+let pasatiempo = "Ayudar"
+let grado = ""
+let materia = ""
+let curso = "Quinto"
+let carreraa
+
+const documento = 102030
+const numfav = 7
+const comidafav = "Pastas"
+const nota = 4.5
+const promedio = 5
+
+let notap = documento === 102030 && nota === 4.5 ? grado = "Aprobado" : grado = "Perdido"
+console.log(notap);
+
+let asignatura = promedio === 5 && curso === "Sexto" ? materia = "C Diferencial" : materia = "Matematicas"
+console.log(asignatura);
+
+let carrera = pasatiempo === "Ayudar" ? materia = "Aprobado" : curso = "Quinto" 
+console.log(carrera);
